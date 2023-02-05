3d0a4f4c479a891eb8ec820275f093bb513d3a08
eduardotecnologo <eduardotecnologo@hotmail.com>
2023-02-05 09:45:28 -0300

    setup VStack/HStack

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Button, HStack, Text, VStack, useToast } from 'native-base';
+import { Box, Button, HStack, Text, VStack, useToast } from 'native-base';
 
 interface IDashboardProps {
     
 }
 
 export const Dashboard: React.FC<IDashboardProps> = () => {
+    const [copoSize, setCopoSize] = useState<number>(250);
     const [water, setWater] = useState<number>(0);
     const [goal, setGoal] = useState<number>(2000);
     const toast = useToast();
 
     const handleWater = () =>{
-        setWater(water + 250);
+        setWater(water + copoSize);
         toast.show({
-            description: "Ual! você bebeu 250ml de água!"
-
+            description: `Ual! você bebeu ${copoSize} ml de água`
         })
     };
+
+    const handleChangeCopo = (size: number) => {
+        setCopoSize(size);
+    }
 
     useEffect(() => {
         if (water >= goal){
@@ -29,16 +33,40 @@ export const Dashboard: React.FC<IDashboardProps> = () => {
     }, [water]);
 
     return (
-        <>
-            <HStack alignItems="center" justifyContent="center">
-            <Text>Faltam</Text>
-            <Text fontSize="6xl">{water}</Text>
-            <Text fontSize="xl">para {goal} ml</Text>
-            </HStack>
-            <Button
-            colorScheme="primary"
-            onPress={handleWater}>Beber água
-            </Button>          
-        </>
+        <VStack flex={1} width='100%' 
+                justifyContent='space-between'
+                alignItems='center'p={4}
+                mt={10}>
+
+            <Text fontSize="sm">
+                {''} volume {copoSize} {''} ml
+            </Text>
+
+            <VStack>
+                <HStack alignItems="center" justifyContent="center">
+                <Text>Faltam</Text>
+                <Text fontSize="6xl">{water}</Text>
+                <Text fontSize="xl">para {goal} ml</Text>
+                </HStack>
+
+                <Button
+                    mt={2}
+                    colorScheme="primary"
+                    onPress={handleWater}>Beber água
+                </Button> 
+            </VStack>
+
+            <Box mt={5}>
+            <Button.Group >
+                <Button onPress={() => handleChangeCopo(250)} 
+                    colorScheme="teal">Copo 250ml</Button>
+                <Button onPress={() => handleChangeCopo(350)} 
+                    colorScheme="teal">Xicára de chá</Button>
+                <Button onPress={() => handleChangeCopo(600)} 
+                    colorScheme="teal">Garrafa de 600ml</Button>
+            </Button.Group> 
+            </Box>
+                
+        </VStack>
    );
 };
